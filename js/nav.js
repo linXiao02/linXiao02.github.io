f65f4fcd06b1dea85ca653ea892a6a3861b6e31f
xiaolin <1849569695@qq.com>
2022-10-20 07:55:24 +0800

    Site updated: 2022-10-20 07:55:21

---
+++ js/nav.js
@@ -0,0 +1,39 @@
+//js有一个小问题：就是只要鼠标滚动不论哪里都会响应，即便你滚动的是子元素
+
+//2022.9.11 已修复，需要jq，请自行引入
+
+
+document.getElementById("name-container").setAttribute("style", "display:none");
+
+var position = $(window).scrollTop();
+
+$(window).scroll(function () {
+
+  var scroll = $(window).scrollTop();
+
+  if (scroll > position) {
+
+
+    document.getElementById("name-container").setAttribute("style", "");
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+
+  } else {
+
+
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+
+  }
+
+  position = scroll;
+
+});
+function scrollToTop(){
+    document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+    document.getElementById("name-container").setAttribute("style","display:none");
+    btf.scrollToDest(0, 500);
+}
+
+
+
+
