43f52bb9679c36d8ac15007ba50ea882270cd845
xiaolin <1849569695@qq.com>
2022-10-20 14:32:41 +0800

    Site updated: 2022-10-20 14:32:38

---
+++ js/custom.js
@@ -0,0 +1,260 @@
+var CURSOR;
+
+Math.lerp = (a, b, n) => (1 - n) * a + n * b;
+
+const getStyle = (el, attr) => {
+    try {
+        return window.getComputedStyle
+            ? window.getComputedStyle(el)[attr]
+            : el.currentStyle[attr];
+    } catch (e) {}
+    return "";
+};
+
+class Cursor {
+    constructor() {
+        this.pos = {curr: null, prev: null};
+        this.pt = [];
+        this.create();
+        this.init();
+        this.render();
+    }
+
+    move(left, top) {
+        this.cursor.style["left"] = `${left}px`;
+        this.cursor.style["top"] = `${top}px`;
+    }
+
+    create() {
+        if (!this.cursor) {
+            this.cursor = document.createElement("div");
+            this.cursor.id = "cursor";
+            this.cursor.classList.add("hidden");
+            document.body.append(this.cursor);
+        }
+
+        var el = document.getElementsByTagName('*');
+        for (let i = 0; i < el.length; i++)
+            if (getStyle(el[i], "cursor") == "pointer")
+                this.pt.push(el[i].outerHTML);
+
+        document.body.appendChild((this.scr = document.createElement("style")));
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='.5'/></svg>") 4 4, auto}`;
+    }
+
+    refresh() {
+        this.scr.remove();
+        this.cursor.classList.remove("hover");
+        this.cursor.classList.remove("active");
+        this.pos = {curr: null, prev: null};
+        this.pt = [];
+
+        this.create();
+        this.init();
+        this.render();
+    }
+
+    init() {
+        document.onmouseover  = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.add("hover");
+        document.onmouseout   = e => this.pt.includes(e.target.outerHTML) && this.cursor.classList.remove("hover");
+        document.onmousemove  = e => {(this.pos.curr == null) && this.move(e.clientX - 8, e.clientY - 8); this.pos.curr = {x: e.clientX - 8, y: e.clientY - 8}; this.cursor.classList.remove("hidden");};
+        document.onmouseenter = e => this.cursor.classList.remove("hidden");
+        document.onmouseleave = e => this.cursor.classList.add("hidden");
+        document.onmousedown  = e => this.cursor.classList.add("active");
+        document.onmouseup    = e => this.cursor.classList.remove("active");
+    }
+
+    render() {
+        if (this.pos.prev) {
+            this.pos.prev.x = Math.lerp(this.pos.prev.x, this.pos.curr.x, 0.15);
+            this.pos.prev.y = Math.lerp(this.pos.prev.y, this.pos.curr.y, 0.15);
+            this.move(this.pos.prev.x, this.pos.prev.y);
+        } else {
+            this.pos.prev = this.pos.curr;
+        }
+        requestAnimationFrame(() => this.render());
+    }
+}
+
+(() => {
+    CURSOR = new Cursor();
+    // 需要重新获取列表时，使用 CURSOR.refresh()
+})();
+
+
+
+
+// 切换背景图片
+
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (t < (time * 60 * 1000) && t > -1) return d.data;
+    }
+    return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+    let data = loadData('blogbg', 1440)
+    if (data) changeBg(data, 1)
+    else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+    let bg = document.getElementById('web_bg')
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+    } else bg.style.backgroundImage = s
+    if (!flag) { saveData('blogbg', s) }
+}
+
+
+
+
+
+// 存数据
+// name：命名 data：数据
+function saveData(name, data) {
+    localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
+}
+
+// 取数据
+// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
+function loadData(name, time) {
+    let d = JSON.parse(localStorage.getItem(name));
+    // 过期或有错误返回 0 否则返回数据
+    if (d) {
+        let t = Date.now() - d.time
+        if (t < (time * 60 * 1000) && t > -1) return d.data;
+    }
+    return 0;
+}
+
+// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
+
+// 读取背景
+try {
+    let data = loadData('blogbg', 1440)
+    if (data) changeBg(data, 1)
+    else localStorage.removeItem('blogbg');
+} catch (error) { localStorage.removeItem('blogbg'); }
+
+// 切换背景函数
+// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
+// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
+function changeBg(s, flag) {
+    let bg = document.getElementById('web_bg')
+    if (s.charAt(0) == '#') {
+        bg.style.backgroundColor = s
+        bg.style.backgroundImage = 'none'
+    } else bg.style.backgroundImage = s
+    if (!flag) { saveData('blogbg', s) }
+}
+
+// 以下为2.0新增内容
+
+// 创建窗口
+var winbox = ''
+
+function createWinbox() {
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    winbox = WinBox({
+        id: 'changeBgBox',
+        index: 999,
+        title: "切换背景",
+        x: "center",
+        y: "center",
+        minwidth: '300px',
+        height: "60%",
+        background: 'var(--leonus-blue)',
+        onmaximize: () => { div.innerHTML = `<style>body::-webkit-scrollbar {display: none;}div#changeBgBox {width: 100% !important;}</style>` },
+        onrestore: () => { div.innerHTML = '' }
+    });
+    winResize();
+    window.addEventListener('resize', winResize)
+
+    // 每一类我放了一个演示，直接往下复制粘贴 a标签 就可以，需要注意的是 函数里面的链接 冒号前面需要添加反斜杠\进行转义
+    winbox.body.innerHTML = `
+    <div id="article-container" style="padding:10px;">
+    
+    <p><button onclick="localStorage.removeItem('blogbg');location.reload();" style="background:#5fcdff;display:block;width:100%;padding: 15px 0;border-radius:6px;color:white;"><i class="fa-solid fa-arrows-rotate"></i> 点我恢复默认背景</button></p>
+    <h2 id="图片（手机）"><a href="#图片（手机）" class="headerlink" title="图片（手机）"></a>图片（手机）</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)" class="pimgbox" onclick="changeBg('url(https\://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)')"></a>
+    </div>
+    
+    <h2 id="图片（电脑）"><a href="#图片（电脑）" class="headerlink" title="图片（电脑）"></a>图片（电脑）</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)" class="imgbox" onclick="changeBg('url(https\://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)')"></a>
+    </div>
+    
+    
+    
+    <h2 id="渐变色"><a href="#渐变色" class="headerlink" title="渐变色"></a>渐变色</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #eecda3, #ef629f)" onclick="changeBg('linear-gradient(to right, #eecda3, #ef629f)')"></a>
+    </div>
+    
+    <h2 id="纯色"><a href="#纯色" class="headerlink" title="纯色"></a>纯色</h2>
+    <div class="bgbox">
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #7D9D9C" onclick="changeBg('#7D9D9C')"></a> 
+    </div>
+`;
+}
+
+// 适应窗口大小
+function winResize() {
+    var offsetWid = document.documentElement.clientWidth;
+    if (offsetWid <= 768) {
+        winbox.resize(offsetWid * 0.95 + "px", "90%").move("center", "center");
+    } else {
+        winbox.resize(offsetWid * 0.6 + "px", "70%").move("center", "center");
+    }
+}
+
+// 切换状态，窗口已创建则控制窗口显示和隐藏，没窗口则创建窗口
+function toggleWinbox() {
+    if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
+    else createWinbox();
+}
+
+
+
+
+
+
+// 网页标题栏
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ你走吧呜呜呜！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ我就知道你会回来！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
