0a65a63614ae69ae1c8ce485a8f013fae4b8d8e1
xiaolin <1849569695@qq.com>
2022-10-20 08:32:20 +0800

    Site updated: 2022-10-20 08:32:16

--- js/nav.js
+++ js/nav.js
@@ -12,27 +12,25 @@ $(window).scroll(function () {
   var scroll = $(window).scrollTop();
 
   if (scroll > position) {
-
-
     document.getElementById("name-container").setAttribute("style", "");
     document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
 
   } else {
-
-
     document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
     document.getElementById("name-container").setAttribute("style", "display:none");
 
   }
-
   position = scroll;
-
 });
 function scrollToTop(){
     document.getElementsByClassName("menus_items")[1].setAttribute("style","");
     document.getElementById("name-container").setAttribute("style","display:none");
     btf.scrollToDest(0, 500);
 }
+
+document.getElementById("page-name").innerText = document.title.split(" | 林のBlog")[0];
+
+
 
 
 
